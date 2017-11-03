a9c271b4709d0b06a5c88dbe1f9ae71998aeae6d
Mandi <ASUS@windows10.microdone.cn>
2017-11-03 01:05:06 +0100

    caregiver login html&css

--- webDemo2/application/views/login.php
+++ webDemo2/application/views/login.php
@@ -7,39 +7,30 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>GraceAge2.0 CAREGIVER</title>
-        <link href="<?php echo base_url()."assets/css/style.css"?>" ref="stylesheet" type = "text/css">
+        <title></title>
+        <link href="<?php echo base_url('/assets/css/style.css')?>" rel="stylesheet" type = "text/css">
     </head>
     <body>
-        <header>
-        <div id="logo">
-          <h1>GraceAge2.0 CAREGIVER <?php echo base_url()."assets/css/style.css"?></h1>
-          <h2></h2>
-        </div>
-        </header>
         <main>
-        <div id ="frm">
+        <div id ="form">
             <form action="process.php" method="POST">
-                <p>
-                    <label>Username:</label>
-                    <input type="text" id="user" name="user"/>
-                </p>
-                <p>
-                    <label>Password:</label>
-                    <input type="password" id="pass" name="pass"/>
-                </p>   
-                <p>
-                    <input type="submit" id="btn" value="login"
-                </p>  
-                <p>
-                    <input type="submit" id="btn2" value="Forget Password? Click here"
-                </p>                  
-            <form/>  
-            </main>
-              <footer>
-        <p>Copyright © 2017 WebApps. Groep T All Rights Reserved.  
-          <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a>
-        </p>
-      </footer>
+        <div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>GRACE AGE<span> Caregiver</span></div>
+		</div>
+		<br>
+		<div class="login">
+				<input type="text" placeholder="username" name="user"><br>
+				<input type="password" placeholder="password" name="password"><br>
+				<input type="button" value="Login">
+                                <input type="button" value="Forget Password? Click here!">
+		</div>
+                <div class="jump">
+                    <input type="button" value="Not caregiver? Click here!">
+                </div>
+            </form>
+        </div>
+        </main>
     </body>
 </html>
