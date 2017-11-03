37d911eb7858ffa4798769aad21e4711798ce0c2
GianiPC <giani.driesen@gmail.com>
2017-11-03 10:04:47 +0100

    fixed base url and linked the styling sheets (done by Dries)

--- webDemo2/application/views/login.php
+++ webDemo2/application/views/login.php
@@ -8,7 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link href="<?php echo base_url('/assets/css/style.css')?>" rel="stylesheet" type = "text/css">
+        <link href="<?php echo base_url();?>/assets/css/style.css" rel="stylesheet" type = "text/css">
     </head>
     <body>
         <main>
