b5d5ad86505dd127e32d1cb8ed114fdc6626a6d8
Mandi <ASUS@windows10.microdone.cn>
2017-11-09 23:11:41 +0100

    css and php upload for questionpage

--- webDemo2/application/views/questionpage.php
+++ webDemo2/application/views/questionpage.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+ 
 <html>
     <head>
         <title>GraceAge 2.0 - Resident</title>
@@ -6,45 +7,51 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel ="stylesheet/less" type="text/css" href="<?php echo base_url();?>/assets/css/QuestionPage.less"/>
+        <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet">
         <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+<!--        <style>@font-face {  
+            /* font-properties */  
+            font-family:caviarDreams;  
+            src:url('/fonts/CaviarDreams.ttf') format('truetype'); /* IE9 */  
+            font-weight: normal; font-style: normal;
+        } </style>-->
     </head>
+    
     <body>
         <div id="content">
-            <div class="page-header">
-                <div class="row">
-                    <h1 class="col-md-8"><font size="20"><b>{topic}</b></font></h1>
-                    <div class ="col-md-4">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-lg btn-primary">Smaller Font</button>
-                            <button type="button" class="btn btn-lg btn-primary">Bigger Font</button>
-                        </div>
+            <div class="page-header" style="background: #2c3d51;">
+                <div class="row" style="background: #2c3d51;">
+                    <h1 class="col-md-8" style="font-family: 'CaviarDreams',sans-serif;color:#e4e4e4;opacity: 20%"><font size="20"><b>{topic}</b></font></h1>
+                     
+                    <div class ="col-md-4" style="display:flex; flex-direction: row">
+                        <h2 type="text"style="font-family: 'Lato', sans-serif; margin-left: 20px; padding-top: 20px;
+                              margin-right: 20px;font-size: 20px;color:#e4e4e4;">Letter Size:</h2>
+                        <button type="button" class="btn btn-lg btn-primary"style="background:#e4e4e4;margin-top: 30px; font-family: 'Lato:900', sans-serif;color:#2c3d51;">Smaller Font</button>
+                        <button type="button" class="btn btn-lg btn-primary"style="background:#e4e4e4;margin-top: 30px;font-family: 'Lato', sans-serif;color:#2c3d51;">Bigger Font</button>
+                        
                     </div>
                 </div>
             </div>
-<!--            <div class="progress">
-                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="3" style="width:33%">
-                    1/3
-                </div>
-            </div>            -->
-            <div id="question" class="container">
-                <p class="text-center"><font size="20">{question}</font></p>
+            <div id="question" class="container"style="color:2c3d51">
+                <p class="text-center" style="font-family: 'Lato', sans-serif;color:#2c3d51"><font size="20">{question}</font></p>
             </div>
             <div class="row">
                 <div class="col-md-1"></div>
                 <div class="col-md-2" align="center">
-                    <button id="b0" style="height:190px; width: 190px;" type="button"><img id="img0" src="https://i.imgur.com/xPJEQwn.png" width="20"/></button>
+                    <button id="b0" style="height:180px; width:  16.7%; border-radius: 20%;" type="button"><img id="img0" src="../../assets/css/image/face.png" width="20"style="border-radius: 20%;"/></button>
                 </div>
                 <div class="col-md-2" align="center">
-                    <button id="b1" style="height:190px; width: 190px;" type="button"><img id="img1" src="https://i.imgur.com/qfPTgD2.png" width="20" /></button>
+                    <button id="b1" style="height:180px; width: 16.7%;border-radius: 20%; " type="button"><img id="img1" src="../../assets/css/image/face1.png" width="20"style="border-radius: 20%; "/></button>
                 </div>
                 <div class="col-md-2" align="center">
-                    <button id="b2" style="height:190px; width: 190px;" type="button"><img id="img2" src="https://i.imgur.com/NCMeAY9.png" width="20" /></button>
+                    <button id="b2" style="height:180px; width: 16.7%;border-radius: 20%;" type="button"><img id="img2" src="../../assets/css/image/face2.png" width="20"style="border-radius: 20%;" /></button>
                 </div>            
                 <div class="col-md-2"align="center">
-                    <button id="b3" style="height:190px; width: 190px;" type="button"><img id="img3" src="https://i.imgur.com/vmZCxFk.png" width="20" /></button>
+                    <button id="b3" style="height:180px; width: 16.7%;border-radius: 20%;" type="button"><img id="img3" src="../../assets/css/image/face3.png" width="20"style="border-radius: 20%;" /></button>
                 </div>
                 <div class="col-md-2" align="center">
-                    <button id="b4" style="height:190px; width: 190px;" type="button"><img id="img4" src="https://i.imgur.com/dtoMky6.png" width="20" /></button>
+                    <button id="b4" style="height:180px; width: 16.7%;border-radius: 20%;" type="button"><img id="img4" src="../../assets/css/image/face4.png" width="20"style="border-radius: 20%; "/></button>
                 </div>
                 <div class="col-md-1"></div>
             </div>
