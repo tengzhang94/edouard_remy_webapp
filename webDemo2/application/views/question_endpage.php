92ad6e4c8b6a207a028e54716ab726feef6da321
GianiPC <giani.driesen@gmail.com>
2017-11-14 13:06:20 +0100

    Topic selection html&css & end of question page html&css

---
+++ webDemo2/application/views/question_endpage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+ 
+<html>
+    <head>
+        <title>GraceAge 2.0 - Resident</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel ="stylesheet/less" type="text/css" href="http://localhost/a17_webApps04/webDemo2/assets/css/Resident.less"/>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+
+    </head>
+
+<body>
+    <p class="text-right">{font_size}
+        <button class="btn btn-primary" type="button" style="margin-left:10px;">{greater} </button>
+        <button class="btn btn-primary" type="button">{smaller} </button>
+    </p>
+    <p class="text-center">{content} </p>
+    <p class="text-center" style="margin-top:120px;">
+        <button class="btn btn-default" type="button">{yes}</button>
+        <button class="btn btn-default" type="button">{no} </button>
+    </p>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ webDemo2/application/views/resident_topicpage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<html>
+    <head>
+        <title>GraceAge 2.0 - Resident</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel ="stylesheet/less" type="text/css" href="http://localhost/a17_webApps04/webDemo2/assets/css/Resident.less"/>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+    </head>
+
+<body>
+    <div>
+        <p class="text-right" id="header"><span class="float_left">Choose a topic</span><span class="float_right">Font size:<button class="btn btn-primary" type="button" style="margin-left:10px;">Greater </button><button class="btn btn-primary" type="button">Smaller </button></span></p>
+        <p
+        class="text-center">
+            <div class="btn-group" role="group">
+                <button class="btn btn-link" type="button" style="margin-left:10px;"><small>All questions</small><img src="assets/css/image/icons8-start.png"></button>
+                <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Meals </small><img src="assets/css/image/icons8-meal.png"></button>
+                <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Comfort </small><img src="assets/css/image/icons8-sofa.png"></button>
+                <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Privacy </small><img src="assets/css/image/icons8-privacy (2).png"></button>
+            </div>
+            </p>
+            <p class="text-center">
+                <div class="btn-group" role="group">
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Autonomy </small><img src="assets/css/image/icons8-clenched-fist.png"></button>
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Respect </small><img src="assets/css/image/icons8-handshake.png"></button>
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Activities </small><img src="assets/css/image/icons8-walking.png"></button>
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Information </small><img src="assets/css/image/icons8-info.png"></button>
+                </div>
+            </p>
+            <p class="text-center">
+                <div class="btn-group" role="group">
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small>Safety </small><img src="assets/css/image/icons8-security-lock.png"></button>
+                    <button class="btn btn-link" type="button" style="margin-left:10px;margin-right:10px;padding-right:0px;padding-left:0px;"><small style="font-size:20px;">Responsivity of</small><small style="font-size:20px;">the caregivers</small><img src="assets/css/image/icons8-communicate.png" style="margin-top:5px;"></button>
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small style="font-size:20px;">Feeling a connection</small><small style="font-size:20px;">with the caregivers</small><img src="assets/css/image/icons8-volunteering.png" style="margin-top:7px;"></button>
+                    <button class="btn btn-link" type="button" style="margin-left:10px;"><small style="font-size:20px;">Contact with</small><small style="font-size:20px;">friends and family</small><img src="assets/css/image/icons8-family.png" style="margin-top:5px;"></button>
+                </div>
+            </p>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
