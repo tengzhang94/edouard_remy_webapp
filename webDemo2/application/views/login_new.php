1d76dd56cc4cfa58ab3dec44401dbf1de6d5d6ea
GianiPC <giani.driesen@gmail.com>
2017-11-16 17:49:03 +0100

    Loginpage fix

--- webDemo2/application/views/login_new.php
+++ webDemo2/application/views/login_new.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-        <link rel ="stylesheet/less" type="text/css" href="<?php echo base_url();?>/assets/css/LoginPage.less"/>
+        <link rel ="stylesheet/less" type="text/css" href="<?php echo base_url();?>assets/css/LoginPage.css"/>
         <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
  </head>
 
@@ -17,7 +17,7 @@
         <h1 class="text-center">GraceAge 2.0</h1>
         <form method="post" action="click">
             <h2 class="sr-only">Login Form</h2>
-            <div class="illustration" style="height:auto;"><img src="assets/img/icons8-lock-100 (1).png" style="margin-top:-40px;"></div>
+            <div class="illustration" style="height:auto;"><img src="<?php echo base_url();?>assets/css/image/icons8-lock.png" style="margin-top:-40px;"></div>
             <div class="form-group" style="height:auto;">
                 <input class="form-control" type="text" name="user" placeholder="{username}" style="font-family:Lato, sans-serif;font-size:18px;">
             </div>
