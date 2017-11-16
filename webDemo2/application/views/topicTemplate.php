b9726d31cfcc5adb20f78a88eb009b219b4863eb
Dries <Dries@dBaumersuni-pc>
2017-11-16 22:05:19 +0100

    added the right icon to each topic in the topic selection page

--- webDemo2/application/views/topicTemplate.php
+++ webDemo2/application/views/topicTemplate.php
@@ -2,7 +2,7 @@
     <div class="form-group">
         <?php foreach($paragraph as $par){
             $topicName = wordwrap($par['topicName'], 20, "<br />");
-            echo '<button class="btn btn-link" name="' . $par['idTopic'] . '" type="submit" style="margin-left:10px;"><small style="font-size:20px;">' . $topicName . '</small><img src="' . base_url() . 'assets/css/image/icons8-start.png"></button>';
+            echo '<button class="btn btn-link" name="' . $par['idTopic'] . '" type="submit" style="margin-left:10px;"><small style="font-size:20px;">' . $topicName . '</small><img src="' . base_url() . 'assets/css/image/icons8-topic-' . $par['idTopic'] .'.png"></button>';
         }?>
     </div>
 <?php echo form_close();?>
