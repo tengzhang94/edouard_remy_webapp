7b16925bfc50de6f32e17130cd2c6c97c9d29526
GianiPC <giani.driesen@gmail.com>
2017-11-19 19:52:44 +0100

    Responsiveness navbar

--- webDemo2/application/views/navbar_topbar.php
+++ webDemo2/application/views/navbar_topbar.php
@@ -8,3 +8,39 @@
         </ul>
     </div>
 </div>
+    
+    
+<!--<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>GraceAge 2.0 - Caregiver</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel ="stylesheet/less" type="text/css" href="<?php echo base_url(); ?>assets/css/navbar.css"/>
+    </head>
+        
+    <body>
+        <p class="text-right" id="header"><span class="float_left">GraceAge 2.0 - <?php echo $title; ?>; </span></p>
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-1 col-md-offset-0">
+                        {menu}
+                        <li class="item {className}">
+                            <a href="{link}"> 
+                                {svg}
+                                <span class="category">{name}</span>
+                            </a>
+                        </li>
+                        {/menu} 
+                    </div>
+                    <div class="col-md-11"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+-->
