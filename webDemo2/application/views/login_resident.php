6efe591774baf0e66baa7b99528412d136581edd
GianiPC <giani.driesen@gmail.com>
2017-11-21 17:34:29 +0100

    Login Resident

---
+++ webDemo2/application/views/login_resident.php
@@ -0,0 +1,23 @@
+<html>
+    
+<head>
+        <title>GraceAge 2.0 - Resident</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel ="stylesheet/less" type="text/css" href="<?php echo base_url();?>assets/css/LoginPage.css"/>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+ </head>
+
+<body>
+    <div class="login-dark">
+        <button class="btn btn-primary btn pull-right" type="submit">{goto_caregiver}</button>
+
+        <form method="post" class="resident"></form>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
+
