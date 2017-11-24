6f3afe6aec3b722fe719ad6e06a4e3036a9baf00
jerem <jerem@10.43.118.168>
2017-11-24 15:16:18 +0100

    final change to the overview page

--- webDemo2/application/views/residents_overview.php
+++ webDemo2/application/views/residents_overview.php
@@ -48,9 +48,10 @@
                                     <button class="btn btn-default" type="submit" style="background-image:url(&quot;<?php echo base_url();?>assets/css/image/icons8-search-100.png&quot;);height:100%;background-position:center;background-size:cover;border-color:transparent;"></button>
                                 </div>
                                 <div class="col-md-12" style="width:46px;padding:0px;margin-left:5%;height:46px;">
-                                    <button class="btn btn-default" type="submit" style="background-image:url(<?php echo base_url();?>assets/css/image/icons8-filter-filled-100.png);height:100%;background-position:center;background-size:cover;border-color:transparent;"></button>
+                                   <button class="btn btn-default" type="submit" style="background-image:url(<?php echo base_url();?>assets/css/image/icons8-filter-filled-100.png);height:100%;background-position:center;background-size:cover;border-color:transparent;"></button> 
+                      
                                 </div>
-                                <form method="post" action="goToAddresident">
+                                <form method="post" action="addResident">
                                 <div class="col-md-12" style="width:46px;padding:0px;margin-left:5%;height:46px;">
                                     <button class="btn btn-default" type="submit" style="background-image:url(&quot;<?php echo base_url();?>assets/css/image/icons8-plus-math-100.png&quot;);height:100%;background-position:center;background-size:cover;border-color:transparent;"></button>
                                    
@@ -61,10 +62,14 @@
                     </div>
                     <div class="row" style="height:90%;">
                         {residents}
-                        <div class="col-md-11" style="padding-right:0;padding-left:0;width:16.6%;height:33%;">
-                            <div style="width:100%;height:100%;">
+                        <div class="col-md-11" style="padding-right:0;padding-left:0;width:16%;height:33%; margin-left: 9%">
+                            <div style="width:100%;height:100%; padding-right: 5%">
                                 <div class="row" style="height:60%;margin-top:17px;">
-                                    <div class="col-md-12" style="width:60%;height:103px;padding:0px;margin-left:10%;"><img src="<?php echo base_url();?>assets/css/image/icons8-customer-50.png" style="width:100%;height:100%;" /></div>
+                                    <div class="col-md-12" style="width:60%;height:103px;padding:0px;margin-left:10%;">
+                                        <button class="btn btn-default" type="button" style="width:100%;height:100%;">
+                                            <img src="<?php echo base_url();?>assets/css/image/icons8-customer-50.png" style="width:100%;height:100%;" />
+                                        </button>
+                                        </div>
                                     <div class="col-md-12" style="width:30%;height:103px;padding:0px;">
                                         <div class="row" style="height:51.5px;">
                                             <div class="col-md-12" style="height:100%;width:100%;padding:0px;"><img src="<?php echo base_url();?>assets/css/image/icons8-happy-100.png" style="width:51.5px;height:100%;" /></div>
@@ -75,8 +80,8 @@
                                     </div>
                                 </div>
                                 <div class="row" style="height:30%;">
-                                    <div class="col-md-12" style="width:45%;padding:0px;margin:0px;margin-left:10%;height:100%;"><span style="width:100%;height:100%;font-size:14px;margin-right:0px;margin-left:30%;">{firstName} {lastName}</span></div>
-                                    <div class="col-md-12" style="width:45%;padding:0px;height:100%;"><span style="width:100%;height:100%;font-size:14px;margin-left:30%;">room {Sectors_idSector}.{roomNr}</span></div>
+                                    <div class="col-md-12" style="width:60%;padding:0px;margin:0px;margin-left:10%;height:100%;"><span style="width:100%;height:100%;font-size:14px;margin-right:0px;margin-left:10%;">{firstName} {lastName}</span></div>
+                                    <div class="col-md-12" style="width:30%;padding:0px;height:100%;"><span style="width:100%;height:100%;font-size:14px;">room {Sectors_idSector}.{roomNr}</span></div>
                                 </div>
                             </div>
                         </div>
