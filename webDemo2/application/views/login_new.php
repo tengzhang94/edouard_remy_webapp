53d0b0bbf9664cb127cff24f3a76ea0d8c79ea13
Kasper <Kasper@10.43.122.198>
2017-11-24 14:04:07 +0100

    Small changes to form validation messages

--- webDemo2/application/views/login_new.php
+++ webDemo2/application/views/login_new.php
@@ -19,13 +19,13 @@
             <h2 class="sr-only">Login Form</h2>
             <div class="illustration" style="height:auto;"><img src="<?php echo base_url();?>assets/css/image/icons8-lock.png" style="margin-top:-40px;"></div>            
             <div class="form-group" style="height:auto;">
-                <p style="color: red;" <?php echo form_error('user'); ?></p>
+                <p id="form_error" <?php echo form_error('user'); ?></p>
                 <?php echo $login_fail; ?></p>
-                <input class="form-control" type="text" name="user" placeholder="{username}" value="<?php echo set_value('user'); ?>" style="font-family:Lato, sans-serif;font-size:18px;">
+                <input id="user_field" class="form-control" type="text" name="user" placeholder="{username}" value="<?php echo set_value('user'); ?>" style="font-family:Lato, sans-serif;font-size:18px;">
             </div>
             <div class="form-group" style="height:auto;">
-                <p style="color: red;" <?php echo form_error('password'); ?></p>
-                <input class="form-control" type="password" name="password" placeholder="{password}" style="font-family:Lato, sans-serif;font-size:18px;">
+                <p id="form_error"  <?php echo form_error('password'); ?></p>
+                <input id="user_field" class="form-control" type="password" name="password" placeholder="{password}" style="font-family:Lato, sans-serif;font-size:18px;">
             </div>
             <div class="form-group" style="height:auto;">
                 <button class="btn btn-primary btn-block" type="submit" style="background-color:#f4f4f4;color:#2c3d51;">{login}</button>
