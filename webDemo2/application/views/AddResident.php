d37da8a8052d1bcbd916cef71cac471d7c3c901c
Teng <>
2017-11-24 13:38:51 +0100

    addResident.less

--- webDemo2/application/views/AddResident.php
+++ webDemo2/application/views/AddResident.php
@@ -27,45 +27,35 @@
         
         <form method="post" action="addResident">
             <div class="column">
-                <div class="form-group" style="height:auto;">
-                    <input class="form-control" type="text" name="firstName" placeholder="firstName" style="font-family:Lato, sans-serif;font-size:18px;">
-<!--                <p class="float_right"><span class="text-right1">Naam: <input type="text" placeholder="naam" name="firstName"></span></p><br>-->
+                <div class="form-group" style="height:55px;">
+      <!--                <p class="float_right"><span class="text-right1">Naam: <input type="text" placeholder="naam" name="firstName"></span></p><br>-->
+                    <p class="float_right"><span class="text_right1">Firstname:<span class="float_right1"><input class="form-control" type="text" name="firstName" placeholder="firstName" ></span></span></p>
                 </div>
-                <div class="form-group" style="height:auto;">
-<!--                <p class="float_right"><span class="text-right1">Achternaam: <input type="text" placeholder="naam" name="lastName"></span></p><br>-->
-                <input class="form-control" type="text" name="lastName" placeholder="lastName" style="font-family:Lato, sans-serif;font-size:18px;">
+                <div class="form-group" style="height:55px;">
+                    <p class="float_right"><span class="text_right1">Lastname:<span class="float_right1"><input class="form-control" type="text" name="lastName" placeholder="lastName" ></span></span></p>
                 </div>
-                <div class="form-group" style="height:auto;">
-<!--                <p class="float_right"><span class="text-right1">Geboortedatum:<input type="text" placeholder="dd/mm/jj" name="birthDate"></span> </p><br>-->
-                <input class="form-control" type="text" name="birthDate" placeholder="birthDate" style="font-family:Lato, sans-serif;font-size:18px;">
+                <div class="form-group" style="height:55px;">
+                    <p class="float_right"><span class="text_right1"> Birthdate:<span class="float_right1"><input class="form-control" type="date" name="birthDate" placeholder="birthDate" min="1917-01-01" max="1980-01-01"></span></span></p>
                 </div>
-                <div class="form-group" style="height:auto;">
-<!--                <p class="float_right"><span class="text-right1">Geslacht: <input type="text" placeholder="geslacht" name="gender"></span></p><br>-->
-                <input class="form-control" type="text" name="gender" placeholder="gender" style="font-family:Lato, sans-serif;font-size:18px;">
+                <div class="form-group" style="height:55px;">
+                    <p class="float_right"><span class="text_right1">Gender:<span class="float_right1"><input type="radio" style="margin-left: -45px;font-size:28px;" name="gender" value="Male" checked>Male<input type="radio" style="margin-left: 40px;font-size:28px;" name="gender" value="Female">Female</span></span></p>
+                </div>                                                                                 
+                <div class="form-group" style="height:55px;">
+                    <p class="float_right"><span class="text_right1">Married:<span class="float_right1"><input type="radio" style="margin-left: -75px;font-size:28px;" name="married" value="Yes" checked>Yes<input type="radio" style="margin-left: 51px;font-size:28px;" name="married" value="No">No</span></span></p>
                 </div>
-                <div class="form-group" style="height:auto;">
-<!--                <p class="float_right"><span class="text-right1">Gehuwd:<input  type="text" placeholder="gehuwd" name="married"></span></p><br>-->
-                <input class="form-control" type="text" name="married" placeholder="married" style="font-family:Lato, sans-serif;font-size:18px;">
-                </div>
-                <div class="form-group" style="height:auto;">
+                <div class="form-group" style="height:55px;">
 <!--                <p class="float_right"><span class="text-right1">Kinderen:<input  type="text" placeholder="kinderen" name="children"></span></p><br>-->
-                <input class="form-control" type="text" name="children" placeholder="children" style="font-family:Lato, sans-serif;font-size:18px;">
+                <p class="float_right"><span class="text_right1">Children:<span class="float_right1"><input type="radio" style="margin-left: -75px;font-size:28px;" name="children" value="Yes" checked>Yes<input type="radio" style="margin-left: 51px;font-size:28px;" name="children" value="No">No</span></span></p>
                 </div>
-                <div class="form-group" style="height:auto;">
+                <div class="form-group" style="height:55px;">
 <!--                <p class="float_right"><span class="text-right1">Anderen: <input  type="text" placeholder="anderen" name="other"></span></p><br>-->
-                <input class="form-control" type="text" name="other" placeholder="other" style="font-family:Lato, sans-serif;font-size:18px;">
+                <p class="float_right"><span class="text_right1">Others:<span class="float_right1"><input class="form-control" type="text" name="others" placeholder="others" ></span></span></p>
                 </div>
-                
-                <p class="text">{success}</p>
-                
-                <div class="form-group" style="height:auto;">
-                    <button class="btn-confirm" type="submit">Submit</button>
-              
-                  
-                  <button class="btn-confirm" type="button" >Cancel</button> 
-                </div>
-
             </div>
+            <div class="form-group" style="height:55px;">
+                    <button class="btn-confirm" type="submit">Submit</button>
+                    <button class="btn-confirm" type="button">Cancel</button>
+                </div>
         </form>        
     </div>
 </body>
