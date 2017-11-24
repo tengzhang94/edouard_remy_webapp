8d125124de43393f5d6f8817742da6c010ec0781
Teng <>
2017-11-24 14:14:03 +0100

    addResident.php temporary

--- webDemo2/application/views/AddResident.php
+++ webDemo2/application/views/AddResident.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
         <link rel ="stylesheet/less" type="text/css" href="<?php echo base_url();?>assets/css/addResident.less"/>
         <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     </head>
 
@@ -25,27 +26,36 @@
         <button class="btn-photo">Photo upload</button>   
         </div>
         
-        <form method="post" action="addResident">
+        <form class="form-horizontal" method="post" action="addResident">
             <div class="column">
-                <div class="form-group" style="height:55px;">
+                <div class="form-group">
       <!--                <p class="float_right"><span class="text-right1">Naam: <input type="text" placeholder="naam" name="firstName"></span></p><br>-->
-                    <p class="float_right"><span class="text_right1">Firstname:<span class="float_right1"><input class="form-control" type="text" name="firstName" placeholder="firstName" ></span></span></p>
+                    <label class="control-label col-sm-2" for="email">Firstname:</label>
+                    <div class="col-sm-10">
+                    <input type="text" class="form-control" id="firstname" placeholder="first name " style="width:200px;"name="firstname">
+                    </div>
                 </div>
-                <div class="form-group" style="height:55px;">
-                    <p class="float_right"><span class="text_right1">Lastname:<span class="float_right1"><input class="form-control" type="text" name="lastName" placeholder="lastName" ></span></span></p>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Lastname:</label>
+                    <div class="col-sm-10">
+                    <input type="text" class="form-control" id="lastname" placeholder="last name " style="width:200px;"name="lasttname">
+                    </div>
                 </div>
-                <div class="form-group" style="height:55px;">
-                    <p class="float_right"><span class="text_right1"> Birthdate:<span class="float_right1"><input class="form-control" type="date" name="birthDate" placeholder="birthDate" min="1917-01-01" max="1980-01-01"></span></span></p>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Birthdate:</label>
+                    <div class="col-sm-10">
+                    <input type="date" class="form-control" id="birthdate" placeholder="dd/mm/jj" style="width:200px;"name="birthdate">
+                    </div>
                 </div>
                 <div class="form-group" style="height:55px;">
                     <p class="float_right"><span class="text_right1">Gender:<span class="float_right1"><input type="radio" style="margin-left: -45px;font-size:28px;" name="gender" value="Male" checked>Male<input type="radio" style="margin-left: 40px;font-size:28px;" name="gender" value="Female">Female</span></span></p>
                 </div>                                                                                 
                 <div class="form-group" style="height:55px;">
-                    <p class="float_right"><span class="text_right1">Married:<span class="float_right1"><input type="radio" style="margin-left: -75px;font-size:28px;" name="married" value="Yes" checked>Yes<input type="radio" style="margin-left: 51px;font-size:28px;" name="married" value="No">No</span></span></p>
+                    <p class="float_right"><span class="text_right1">Married:<span class="float_right1"><input type="radio" style="margin-left: -75px;font-size:28px;" name="married" value="1" checked>Yes<input type="radio" style="margin-left: 51px;font-size:28px;" name="married" value="0">No</span></span></p>
                 </div>
                 <div class="form-group" style="height:55px;">
 <!--                <p class="float_right"><span class="text-right1">Kinderen:<input  type="text" placeholder="kinderen" name="children"></span></p><br>-->
-                <p class="float_right"><span class="text_right1">Children:<span class="float_right1"><input type="radio" style="margin-left: -75px;font-size:28px;" name="children" value="Yes" checked>Yes<input type="radio" style="margin-left: 51px;font-size:28px;" name="children" value="No">No</span></span></p>
+                <p class="float_right"><span class="text_right1">Children:<span class="float_right1"><input type="radio" style="margin-left: -75px;font-size:28px;" name="children" value="1" checked>Yes<input type="radio" style="margin-left: 51px;font-size:28px;" name="children" value="0">No</span></span></p>
                 </div>
                 <div class="form-group" style="height:55px;">
 <!--                <p class="float_right"><span class="text-right1">Anderen: <input  type="text" placeholder="anderen" name="other"></span></p><br>-->
