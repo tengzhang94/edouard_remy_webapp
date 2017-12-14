a781b5bcfe6346d77b5a065edf6f1ed1e696a3f0
kobevf <kobevanfrachem@hotmail.com>
2017-12-14 19:58:08 +0100

    Notifications get automatically generated about when a topic is filled in and when the average is too low.

--- webDemo2/application/models/Notification_model.php
+++ webDemo2/application/models/Notification_model.php
@@ -6,26 +6,28 @@ class Notification_model extends CI_Model {
     }
     
     public function lowAverageNot($residentId, $topicId) {
-        $residentInfo = $this->db->query("SELECT (firstName, lastName, Sectors_idSector) FROM Resident WHERE idResident = '$residentId'")->result_array();
+        $residentInfo = $this->db->query("SELECT firstName, lastName, Sectors_idSector FROM Resident WHERE idResident = '$residentId'")->result_array();
         $topicName = $this->db->query("SELECT * FROM Topics WHERE idTopic = '$topicId'")->result_array();
+        $msgTxt = $residentInfo[0]["firstName"] ." " .$residentInfo[0]["lastName"] ." heeft een lage gemiddelde score gegeven op de categorie: " .$topicName[0]["topicDutch"] .".";
         $not_data = array(
-            'idResident' => $residentId,
-            'Sectors_idSector' => $residentInfo['Sectors_idSector'],
-            'messageText' => $residentInfo['firstName'] + " " + $residentInfo['lastName'] = "heeft een lage gemiddelde score gegeven op de categorie: " + $topicName['topicDutch'] + ".",
-            'priority' => 5,
-            'notificationType' => 'lowScore');
+            "idResident" => $residentId,
+            "Sectors_idSector" => $residentInfo[0]['Sectors_idSector'],
+            "messageText" => $msgTxt,
+            "priority" => 5,
+            "notificationType" => "lowScore");
         $this->db->insert('Notifications', $not_data);
     }
     
     public function topicNot($residentId, $topicId) {
-        $residentInfo = $this->db->query("SELECT (firstName, lastName, Sectors_idSector) FROM Resident WHERE idResident = '$residentId'")->result_array();
+        $residentInfo = $this->db->query("SELECT firstName, lastName, Sectors_idSector FROM Resident WHERE idResident = '$residentId'")->result_array();
         $topicName = $this->db->query("SELECT * FROM Topics WHERE idTopic = '$topicId'")->result_array();
+        $msgTxt = $residentInfo[0]["firstName"] ." " .$residentInfo[0]["lastName"] ." heeft feedback gegeven over: " .$topicName[0]["topicDutch"] .".";
         $not_data = array(
-            'idResident' => $residentId,
-            'Sectors_idSector' => $residentInfo['Sectors_idSector'],
-            'messageText' => $residentInfo['firstName'] + " " + $residentInfo['lastName'] = "heeft feedback gegeven over: " + $topicName['topicDutch'] + ".",
-            'priority' => 0,
-            'notificationType' => 'activity');
+            "idResident" => $residentId,
+            "Sectors_idSector" => $residentInfo[0]["Sectors_idSector"],
+            "messageText" => $msgTxt,
+            "priority" => 0,
+            "notificationType" => "activity");
         $this->db->insert('Notifications', $not_data);
     }
-}
+}//$residentInfo[0]->firstName + " " + $residentInfo[0]->lastName = "heeft feedback gegeven over: " + $topicName[0]->topicDutch + "."
