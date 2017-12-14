9e72647dbc57d60ccf8d316860eb43593b0ff37c
Teng <>
2017-12-14 22:55:36 +0100

    addResident update but not finished

--- webDemo2/application/views/AddResident.php
+++ webDemo2/application/views/AddResident.php
@@ -1,18 +1,14 @@
 <div class="container-fluid">
+    <!--<form class="form-horizontal" method="post" action="addResidentConfirm"> -->
     <form class="form-horizontal" method="post" action="addResidentConfirm">
     <div class="row">
         <div class="col-sm-6 col-xs-12" >
-            
-             <!--<button class="btn-photo">{uploadPhoto}</button>  -->   
+             <!--<img class="btn-photo" id="output" /> -->
+             <image class="btn-photo" name="photo" id="output"/>
+             <input type="file" accept="image/*" onchange="loadFile(event)" style="width:89px;" />
+             <!--<label class="control-label" for="firstname"><image class="btn-photo" id="output">{photo}</image></label> -->
+                    
              
-             <?php echo form_open_multipart('UploadController/do_upload');?>
-             <span id="label_span"></span>
-             <label for="file" class="btn-photo" style="background-position: center; background-size: cover; font-size: 35px;">
-                 <input id="file"  name="userfile" type="file" size="20" style="display: none; ">
-                 <p style="font-family: 'Lato', sans-serif;font-size: 30px;margin-right: 8% ;margin-top:180px;text-align: center;">{uploadPhoto}</p>
-                 </input>
-             </label>  
-             <input id="upload_button" type="submit" style="display: none;" />
              
              <div class="row">
              <div class="form-group"style="height:55px;">
@@ -24,8 +20,7 @@
                 </div>
             </div>
              </div>
-        </div>
-        
+        </div>    
             <div class="col-sm-6 col-xs-12">
                 <div class="form-group" style="margin-top: 70px;">
                     <label class="control-label col-sm-4" for="firstname"><p class="residentInfoStyle">{firstName}:</p></label>
@@ -91,6 +86,15 @@
                     </div>
                 </div>
             </div>
-    </div>    
-    </form>   
+    </div>
+</form>
+       
 </div>
+
+<script>
+  var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+  };
+</script>
+
