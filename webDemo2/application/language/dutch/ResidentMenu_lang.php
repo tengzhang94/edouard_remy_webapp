163e984d04d0f82c793a1a0cefda0a98c6b91549
Dries <Dries@poing-vlan276.kulnet.kuleuven.be>
2017-12-17 23:46:46 +0100

    updated language

--- webDemo2/application/language/dutch/ResidentMenu_lang.php
+++ webDemo2/application/language/dutch/ResidentMenu_lang.php
@@ -3,7 +3,7 @@ $lang['residentMenu_fontSize']="Lettergrootte:";
 $lang['residentMenu_greater']="Groter";
 $lang['residentMenu_smaller']="Kleiner";
 $lang['residentMenu_sentence1']="Wat wil je nu doen? Kies een van de onderstaande opties.";
-$lang['residentMenu_sentence2']="Bekijk je foto's - Er zijn foto's beschikbaar!";
+$lang['residentMenu_sentence2']="Bekijk je berichten - Er zijn berichten beschikbaar!";
 $lang['residentMenu_sentence3']="Beantwoordt meer vragen";
 $lang['residentMenu_sentence4']="Afsluiten";
      
