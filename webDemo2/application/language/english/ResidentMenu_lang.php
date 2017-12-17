1f1e159a48d1cf2f8056a985871fa5f9c7aeecb2
jerem <jerem@10.43.118.168>
2017-12-17 17:07:55 +0100

    add the english version residentMenu_lang

---
+++ webDemo2/application/language/english/ResidentMenu_lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang['residentMenu_fontSize']="Font size:";
+$lang['residentMenu_greater']="Bigger";
+$lang['residentMenu_smaller']="Smaller";
+$lang['residentMenu_sentence1']="What do you want to do now? Select one of the options below.";
+$lang['residentMenu_sentence2']="View your photos - Photos are available!";
+$lang['residentMenu_sentence3']="Answer more questions";
+$lang['residentMenu_sentence4']="Logout";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
