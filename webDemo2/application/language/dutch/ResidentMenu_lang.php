d6df83550ff31984d1d83598edce9e31f375949f
Dries <Dries@poing-vlan276.kulnet.kuleuven.be>
2017-12-18 00:09:31 +0100

    resident menu page behaviour fixed if no messages are available

--- webDemo2/application/language/dutch/ResidentMenu_lang.php
+++ webDemo2/application/language/dutch/ResidentMenu_lang.php
@@ -3,9 +3,10 @@ $lang['residentMenu_fontSize']="Lettergrootte:";
 $lang['residentMenu_greater']="Groter";
 $lang['residentMenu_smaller']="Kleiner";
 $lang['residentMenu_sentence1']="Wat wil je nu doen? Kies een van de onderstaande opties.";
-$lang['residentMenu_sentence2']="Bekijk je berichten - Er zijn berichten beschikbaar!";
-$lang['residentMenu_sentence3']="Beantwoordt meer vragen";
-$lang['residentMenu_sentence4']="Afsluiten";
+$lang['residentMenu_messagesAvailable']="Bekijk je berichten - Er zijn berichten beschikbaar!";
+$lang['residentMenu_noMessages']="Je hebt momenteel geen berichten";
+$lang['residentMenu_moreQuestions']="Beantwoordt meer vragen";
+$lang['residentMenu_logout']="Afsluiten";
      
           
 
